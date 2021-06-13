2de0f8f62619820edfd3cfc344c2610a7c3a3442
Gil Alvaro <gil.pita@gmail.com>
2021-06-13 13:14:37 +0100

    fix storybook

--- packages/storybook/.storybook/main.js
+++ packages/storybook/.storybook/main.js
@@ -7,5 +7,26 @@ module.exports = {
         '@storybook/addon-actions',
         'storybook-addon-i18n',
         'storybook-addon-styled-component-theme'
-    ]
+    ],
+    webpackFinal: async (config) => {
+        config.module.rules.push({
+          test: /\.(ts|tsx)$/,
+          loader: require.resolve('babel-loader'),
+          options: {
+            plugins: [
+                ['@babel/transform-react-jsx', { loose: true }],
+                ['@babel/plugin-proposal-decorators', { legacy: true }],
+                ['@babel/plugin-proposal-class-properties', { loose : true }],
+                ['@babel/proposal-object-rest-spread', { loose: true }]
+             ],
+            presets: [
+                ['@babel/preset-react', { flow: false, typescript: true, loose: true}],
+                ['@babel/preset-typescript', { loose: true }],
+                ['@babel/preset-env', { loose: true }]
+            ],
+          },
+        });
+        config.resolve.extensions.push('.ts', '.tsx');
+        return config;
+    },
 };
