91ee968ca1071e8a58d500c3ce6ba1acb8e5c14d
Gil Alvaro <gil.pita@gmail.com>
2021-06-13 18:11:49 +0100

    Affix fixes

--- packages/ui-kit/src/Atoms/Affix/Affix.stories.tsx
+++ packages/ui-kit/src/Atoms/Affix/Affix.stories.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from "react";
 import { withKnobs, text, boolean, number, color } from "@storybook/addon-knobs";
-import Affix from '.';
+import React, { useState } from 'react';
+import Affix from './Affix';
+import Button from '../../Molecules/Button';
 
 export default {
   title: "Atoms/Affix",
@@ -9,12 +10,24 @@ export default {
 };
 
 
-export const affix  = () => {
+export const AffixDemo: React.FC = () => {
+  const [top, setTop] = useState(10);
+  const [bottom, setBottom] = useState(10);
 
   return (
-    <React.Fragment>
-    <Affix>H</Affix>
-  </React.Fragment>
+    <>
+      <Affix offsetTop={120} onChange={affixed => console.log(affixed)}>
+        <Button type="primary" onClick={() => setTop(top + 10)}>
+          Affix top
+        </Button>
+      </Affix>
+      <br />
+      <Affix offsetBottom={bottom}>
+        <Button type="primary" onClick={() => setBottom(bottom + 10)}>
+          Affix bottom
+        </Button>
+      </Affix>
+    </>
   );
 };
 
